aca6b9e3950242a1c0337553f98dfdf3405cdaad
Anjali Chourasia <anjalichourasia636@gmail.com>
2025-01-14 15:24:21 +0530

    first-commit

--- src/components/InnerComponent/Home/Empowering.tsx
+++ src/components/InnerComponent/Home/Empowering.tsx
@@ -18,7 +18,7 @@ const Empowering = () => {
       {/* Text Content */}
       <div className="relative z-10 flex h-full items-center justify-center px-4 text-center">
         <h1 className="max-w-4xl text-4xl font-bold leading-tight text-white sm:text-5xl md:text-4xl lg:text-5xl">
-          Empowering Individuals to Capture Unforgettable Moments
+          Empowering Individuals to Capture Unforgettable Moment
         </h1>
       </div>
     </section>
